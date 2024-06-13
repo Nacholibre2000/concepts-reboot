817226c048526f25c8bdbd319a007f78d591e52c
Nacholibre2000 <jonatan.blomberg20@gmail.com>
2024-06-13 22:40:27 +0200

    continued eslint headaches

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -21,15 +21,12 @@ module.exports = {
     },
   },
   settings: { react: { version: 'detect' } },
-  plugins: ['react-refresh', '@typescript-eslint', 'jsx-a11y', 'prettier', 'tailwindcss'],
+  plugins: ['react-refresh', '@typescript-eslint', 'jsx-a11y', 'tailwindcss','prettier'],
   rules: {
     'react/react-in-jsx-scope': 'off',
     'react/jsx-no-target-blank': 'off',
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
     'prettier/prettier': ['warn', { singleQuote: true }],
-    'tailwindcss/classnames-order': 'warn',
-    'tailwindcss/enforces-shorthand': 'warn',
-    'tailwindcss/no-custom-classname': 'off',
   },
 };
