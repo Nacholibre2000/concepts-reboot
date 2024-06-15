f8de0cac667d2e8a949c0622d3a5a9547b6cfbab
Nacholibre2000 <jonatan.blomberg20@gmail.com>
2024-06-15 22:56:09 +0200

    further small fixes

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -21,11 +21,20 @@ module.exports = {
     },
   },
   settings: { react: { version: 'detect' } },
-  plugins: ['react-refresh', '@typescript-eslint', 'jsx-a11y', 'tailwindcss','prettier'],
+  plugins: [
+    'react-refresh',
+    '@typescript-eslint',
+    'jsx-a11y',
+    'tailwindcss',
+    'prettier',
+  ],
   rules: {
     'react/react-in-jsx-scope': 'off',
     'react/jsx-no-target-blank': 'off',
-    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
     '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
     'prettier/prettier': ['warn', { singleQuote: true }],
   },
