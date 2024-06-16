88d46081cedbe7183abebbad99193900013e83c8
Nacholibre2000 <jonatan.blomberg20@gmail.com>
2024-06-16 14:09:14 +0200

    frontend connectivity to backend established & curriculum tree is functional

--- frontend/components/Layout.tsx
+++ frontend/components/Layout.tsx
@@ -1,6 +1,6 @@
 import React, { ReactNode } from 'react';
 import Navbar from './Navbar'; // Adjust the import to your folder structure
-/* import Sidebar from './Sidebar'; // Import the Sidebar component */
+import Sidebar from './Sidebar'; // Import the Sidebar component
 
 type LayoutProps = {
   children: ReactNode;
@@ -11,7 +11,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
     <div className="flex h-screen flex-col">
       <Navbar />
       <div className="flex flex-1">
-        {/* <Sidebar /> */}
+        <Sidebar />
         <main>{children}</main>
       </div>
     </div>
