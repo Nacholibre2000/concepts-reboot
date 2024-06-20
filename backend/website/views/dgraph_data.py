70a563b493e1b46cfcb517a57ebc88f1fb4680d7
Nacholibre2000 <jonatan.blomberg20@gmail.com>
2024-06-20 13:37:18 +0200

    rough add node functionality added on the backend

--- backend/website/views/dgraph_data.py
+++ backend/website/views/dgraph_data.py
@@ -53,6 +53,108 @@ def add_node():
         txn.discard()
         client_stub.close()
 
+
+@dgraph_data.route('/add_node', methods=['POST'])
+def add_node():
+    data = request.json
+    image_url = data.get('image_url')
+    concept = data.get('name')
+    person = data.get('person')
+    event = data.get('event')
+    place_name = data.get('place_name')
+    explanation = data.get('explanation')
+    place_geo = data.get('place_geo')  # Assuming this is GeoJSON format
+    date = data.get('date')
+    source = data.get('source')
+    license = data.get('license')
+    hashtags = data.get('hashtags', [])  # List of hashtag objects
+
+    client, client_stub = create_dgraph_client()
+    txn = client.txn()
+
+    try:
+        # Prepare the main node
+        main_node = {
+            'uid': '_:newNode',
+            'image_url': image_url,
+            'concept': concept,
+            'person': person,
+            'event': event,
+            'place_name': place_name,
+            'explanation': explanation,
+            'place_geo': place_geo,
+            'date': date,
+            'source': source,
+            'license': license,
+        }
+
+        # Prepare mutations for hashtags
+        hashtag_uids = []
+        for tag in hashtags:
+            uid = tag.get('uid')
+            if uid:  # Existing node by UID
+                hashtag_uids.append({'uid': uid})
+            else:  # New or existing node by concept, person, event, or place
+                query = None
+                if tag.get('concept'):
+                    query = """
+                    {
+                        existingNode as var(func: eq(concept, "%s"))
+                    }
+                    """ % tag.get('concept')
+                elif tag.get('person'):
+                    query = """
+                    {
+                        existingNode as var(func: eq(person, "%s"))
+                    }
+                    """ % tag.get('person')
+                elif tag.get('event'):
+                    query = """
+                    {
+                        existingNode as var(func: eq(event, "%s"))
+                    }
+                    """ % tag.get('event')
+                elif tag.get('place_name'):
+                    query = """
+                    {
+                        existingNode as var(func: eq(place, "%s"))
+                    }
+                    """ % tag.get('place')
+
+                if query:
+                    response = txn.query(query)
+                    result = json.loads(response.json)
+                    if result['existingNode']:
+                        # Use the existing node UID
+                        hashtag_uids.append({'uid': result['existingNode'][0]['uid']})
+                    else:
+                        # Create a new node
+                        new_tag = {
+                            'uid': f'_:newTag{len(hashtag_uids)}',  # Create unique blank node identifier
+                            'concept': tag.get('concept'),
+                            'event': tag.get('event'),
+                            'person': tag.get('person'),
+                            'place_name': tag.get('place_name')
+                        }
+                        hashtag_uids.append(new_tag)
+
+        # Link hashtags to the main node
+        if hashtag_uids:
+            main_node['hashtags'] = hashtag_uids
+
+        # Perform the mutation
+        mutation = txn.create_mutation(set_obj=main_node)
+        request = txn.create_request(mutations=[mutation], commit_now=True)
+        response = txn.do_request(request)
+        txn.commit()
+
+        return jsonify({'message': 'Node added successfully', 'uid': response.uids['newNode']})
+    except Exception as e:
+        return jsonify({'message': 'Failed to add node', 'error': str(e)}), 500
+    finally:
+        txn.discard()
+        client_stub.close()
+
 @dgraph_data.route('/dgraph/query', methods=['GET'])
 def query_node():
     node_value = request.args.get('node')
